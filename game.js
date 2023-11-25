f19607397ec3f46f9b492f76ae65f3f789c209b4
Nuel Nonaxce <agsisasleep@gmail.com>
2023-11-25 21:53:43 +0800

    Revised everything from scratch
    Replace 90% of the lines and 100% of the JS

--- game.js
+++ game.js
@@ -1,120 +1,150 @@
 // DOM
 // 25/11/2023 11:40AM PH Time
-const gameMap = document.getElementById("game-map");
-let player = {
-    "xPos": 4,
-    "yPos": 4
-};
-// this is will be array version of the code map
-const map2d = [
-//   0      1     2     3     4     5     6     7    8   
-    ["* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "/"], // 0
-    ["* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "/"], // 1
-    ["* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "/"], // 2
-    ["* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "/"], // 3
-    ["* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "/"], // 4
-    ["* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "/"], // 5
-    ["* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "/"], // 6
-    ["* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "/"], // 7
-    ["* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "* ", "/"]  // 8
+// Last edit 25/11/2023 9:41 PM PH Time
+const map1 = document.getElementById("map");
+const addPlayerBtn = document.getElementById("add-player");
+// the tilemap that renders
+let map = [
+    ['0 ', "0 ", "0 ", "0 ", "0 ", "0 ", "0 "],
+    ["0 ", "0 ", "0 ", "0 ", "0 ", "0 ", "0 "],
+    ['0 ', "0 ", '0 ', '0 ', '0 ', "0 ", "0 "],
+    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 "],
+    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 "],
+    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 "],
+    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 "]
+    
 ];
-// CONSOLE BASED GAME**
-console.log(`length/Rows: ${map2d[0].length-1}`);
-console.log(`height/Columns: ${map2d.length}`);
-let output = ``;
-let newLine = "\n";
 
-/*
-function drawMapConsole(){
-    for(let row = 0; row < map2d.length; row++) {
-        //output = ``;
-        for(let col = 0; col < map2d[row].length; col++) {
-            if ((col + 1) == map2d[row].length) {
-                output += newLine;
-            } else {
-                output += map2d[row][col];
-                map2d[4][4] = "X ";
-            }
-        }
-    }
-}
-*/
+// cloned array prevents original from changing
+// idk if this is necessary 
+let newMap = [...map];
 
-// Draws the main map of the game
-// Also contains the player
-// NOTE TO SELF: figure out a way to seperate the player and map (probably create another tilemap above)
-function drawMap(){
-    // reset
-    output = "";
-    removeAllSquares();
-    for(let row = 0; row < map2d.length; row++) {
-        for(let col = 0; col < map2d[row].length; col++) {
-            if ((col + 1) == map2d[row].length) {
-                output += newLine;
-                let BR = document.createElement("br")
-                gameMap.appendChild(BR);
-            } else {
-                let square = document.createElement("div")
-                square.classList.add("square");
-                // changes the squares colo(u)rs to make it look checkered
-                if ((col + 1)%2 == 0) {
-                    square.classList.add("evenSquare");
+// dimensions of the array
+let lenRow = map.length;
+let lenCol = map[0].length;
+console.log(`# of Rows:${lenRow} # of Columns:${lenCol}`);
+
+let tilemap = "";
+function draw() {
+    removeAllChildren();
+    for (let row = 0; row < lenRow; row++) {
+        for (let col = 0; col < lenCol; col++) {
+            let tile = document.createElement("div");
+            // checks if the value on current indexes is "1 " which signifies a player element
+            if (!(newMap[row][col] === "1 ")) {
+                // creates a checkered pattern (of course it gonna look good)
+                if (row%2 == 0) {
+                    if (!(col%2 == 0)) {
+                        tile.classList.add("dark-tile");
+                    } else {
+                        tile.classList.add("light-tile");
+                    }
                 } else {
-                    square.classList.add("oddSquare");
+                    if (col%2 == 0) {
+                        tile.classList.add("dark-tile");
+                    } else {
+                        tile.classList.add("light-tile");
+                    }
                 }
-                // places the player at a specified area
-                // if (col == player.xPos && row == player.yPos) {
-                //     drawPlayer(player.xPos, player.yPos);
-                //     output += map2d[row][col];
-                
-                output += map2d[row][col];
-                gameMap.appendChild(square);
-                
+                map1.appendChild(tile);
+            } else {
+                // If it ever does find the player it will create a player and append
+                let playerElem = document.createElement("div");
+                playerElem.classList.add("player");
+                map1.appendChild(playerElem);
             }
+            
         }
     }
-    console.log(output);
-    console.log("===============")
-    //drawMapConsole();
 }
+// console.logs the array (basically for debugging purposes only)
+function render() {console.log(newMap);}
 
-
-// Removes all the squares of the gameMap
-function removeAllSquares() {
-    while (gameMap.firstChild) {
-        gameMap.removeChild(gameMap.firstChild);
+// removes all child elements 
+function removeAllChildren() {
+    while(map1.firstChild) {
+        map1.removeChild(map1.firstChild);
     }
 }
-
-// Draws the player on the document
-function drawPlayer(y, x) {
-    map2d[y][x] = "X ";
-    let playerElem = document.createElement("div");
-    playerElem.classList.add("playerSquare");
-    gameMap.appendChild(playerElem);
+// stores player position on the map
+let playerPos = {
+    "x": null,
+    "y": null
+}
+// adds the player at a specified point 
+function addPlayer(y, x) {
+    newMap[y][x] = "1 ";
+    playerPos.x = x;
+    playerPos.y = y;
+    draw();
+    render();
+}
+// determines the direction of the 
+function movePlayer(direction) {
+    // changes previous postion to "0 " or an occupied space
+    newMap[playerPos.y][playerPos.x] = "0 "
+    switch(direction) {
+        case "up":
+            // the if statement is for checking if the "player" will go off the array
+            // in this world, its flat
+            if (!(playerPos.y == 0)) {
+                playerPos.y -= 1;
+            } else {console.log("nope");}
+            break;
+        case "right":
+            if (!(playerPos.x == lenCol-1)) {
+                playerPos.x += 1;
+            } else {console.log("nope");}
+            break;
+        case "down":
+            if (!(playerPos.y == lenRow-1)) {
+                playerPos.y += 1;
+            } else {console.log("nope");}
+            break;
+        case "left":
+            if (!(playerPos.x == 0)) {
+                playerPos.x -= 1;
+            } else {console.log("nope");}
+            break;
+        default:
+            alert(
+            `And there he goes! Look a giant turtle! 
+            (iT bROKE AGAIn!)
+            
+            yayyy
+            `
+            );
+    }
+    // to place the player in the new position
+    newMap[playerPos.y][playerPos.x] = "1 "
+    draw();
+    render();
 }
 
-function moveUp() {
-    player.yPos = player.yPos - 1;
-}
-function moveLeft() {
-    player.xPos = player.xPos - 1;
-}
+// render to dom
 
-document.addEventListener('keypress', function(event){
-    if (event.key === "W") {
-        drawPlayer(player.yPos, player.xPos);
-        drawMap();
-        console.log("hi")
+addPlayerBtn.addEventListener('click', () => {
+    addPlayer(1, 2);
+});
+
+// handles key presses
+document.addEventListener('keypress', function(e){
+    if (e.key == "w") {
+        movePlayer("up");
+    } else if (e.key == "d") {
+        movePlayer("right");
+    } else if (e.key == "s") {
+        movePlayer("down");
+    } else if (e.key == "a") {
+        movePlayer("left");
+    } else {
+        console.log("yes");
     }
 });
 
-// THE SOURCE OF ALL THINGS THAT WORK AND MAY WORK AND DO NOT WORK SO DON'T TOUCH OR THIS WON'T WORK ~turtle
-drawMap();
-drawPlayer(player.yPos, player.xPos);
-moveLeft();
-moveUp();
-drawMap()
+// you have reached the end!
+
+
 
 
 
