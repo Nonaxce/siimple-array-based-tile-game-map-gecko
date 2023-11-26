f6bf51b82bd19231f46db999f93d3c1d220b4c19
Nuel Nonaxce <agsisasleep@gmail.com>
2023-11-26 10:19:08 +0800

    Memory Optimization and updated JS
    set the lastChild element to "null" for GC to remove it from memory resulting from up to 30mb to 4.7mb

--- game.js
+++ game.js
@@ -1,19 +1,26 @@
-// DOM
+// Project
 // 25/11/2023 11:40AM PH Time
 // Last edit 25/11/2023 9:41 PM PH Time
 const map1 = document.getElementById("map");
 const addPlayerBtn = document.getElementById("add-player");
+const addBombBtn = document.getElementById("add-explosive");
 // the tilemap that renders
+// "0"s represent empty areas that an object or entity is not being occupied by
+// any value that isn't a 0 will be classified as occupied
 let map = [
-    ['0 ', "0 ", "0 ", "0 ", "0 ", "0 ", "0 ", "0 ", "0 "],
-    ["0 ", "0 ", "0 ", "0 ", "0 ", "0 ", "0 ", "0 ", "0 "],
-    ['0 ', "0 ", '0 ', '0 ', '0 ', "0 ", "0 ", "0 ", "0 "],
-    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 ", "0 ", "0 "],
-    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 ", "0 ", "0 "],
-    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 ", "0 ", "0 "],
-    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 ", "0 ", "0 "],
-    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 ", "0 ", "0 "],
-    ['0 ', '0 ', '0 ', '0 ', '0 ', "0 ", "0 ", "0 ", "0 "]
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     
 ];
 
@@ -28,12 +35,25 @@ console.log(`# of Rows:${lenRow} # of Columns:${lenCol}`);
 
 let tilemap = "";
 function draw() {
-    removeAllChildren();
+    // resets child
+    if (map1.firstChild) removeAllChildren();
     for (let row = 0; row < lenRow; row++) {
         for (let col = 0; col < lenCol; col++) {
             let tile = document.createElement("div");
             // checks if the value on current indexes is "1 " which signifies a player element
-            if (!(newMap[row][col] === "1 ")) {
+            if (newMap[row][col] === 1) {
+                // If it ever does find the player it will create a player and append
+                let playerElem = document.createElement("div");
+                playerElem.classList.add("player");
+                playerElem.setAttribute('title', 'player');
+                map1.appendChild(playerElem);
+            } else if (newMap[row][col] === 2) {
+                // creates a bomb
+                let bombElem = document.createElement("div");
+                bombElem.classList.add("bomb");
+                bombElem.setAttribute('title', 'me bomb');
+                map1.appendChild(bombElem);
+            } else {
                 // creates a checkered pattern (of course it gonna look good)
                 if (row%2 == 0) {
                     if (!(col%2 == 0)) {
@@ -48,12 +68,9 @@ function draw() {
                         tile.classList.add("light-tile");
                     }
                 }
+                map1.setAttribute('title', 'yummy grass');
                 map1.appendChild(tile);
-            } else {
-                // If it ever does find the player it will create a player and append
-                let playerElem = document.createElement("div");
-                playerElem.classList.add("player");
-                map1.appendChild(playerElem);
+                
             }
             
         }
@@ -65,7 +82,9 @@ function render() {console.log(newMap);}
 // removes all child elements 
 function removeAllChildren() {
     while(map1.firstChild) {
-        map1.removeChild(map1.firstChild);
+        // deletes all references to child element to optimize memory
+        map1.lastChild = null;
+        map1.removeChild(map1.lastChild);
     }
 }
 // stores player position on the map
@@ -76,7 +95,7 @@ let playerPos = {
 // adds the player at a specified point 
 let player = false;
 function addPlayer(y, x) {
-    newMap[y][x] = "1 ";
+    newMap[y][x] = 1;
     playerPos.x = x;
     playerPos.y = y;
     draw();
@@ -85,9 +104,9 @@ function addPlayer(y, x) {
 }
 // determines the direction of the 
 function movePlayer(direction) {
-    // changes previous postion to "0 " or an occupied space
+    // changes previous postion to "0 " or an unoccupied space
     if (player) {
-       newMap[playerPos.y][playerPos.x] = "0 "
+       newMap[playerPos.y][playerPos.x] = 0;
         switch(direction) {
             case "up":
                 // the if statement is for checking if the "player" will go off the array
@@ -121,7 +140,7 @@ function movePlayer(direction) {
                 );
         }
         // to place the player in the new position
-        newMap[playerPos.y][playerPos.x] = "1 "
+        newMap[playerPos.y][playerPos.x] = 1;
         draw();
         //render(); 
     } else {
@@ -129,10 +148,27 @@ function movePlayer(direction) {
     }
 }
 
+function addBombs() {
+    //newMap[0][Math.random(12)]
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min);
+    }
+    newMap[0][getRandomInt(0, 13)] = 2;
+    console.log("Bomb dropped");
+}
+addBombs();
+
 // render to dom
 
 addPlayerBtn.addEventListener('click', () => {
     addPlayer(1, 2);
+});
+
+addBombBtn.addEventListener('click', () => {
+    addBombs();
+    draw();
 });
 
 // handles key presses
@@ -145,6 +181,9 @@ document.addEventListener('keypress', function(e){
         movePlayer("down");
     } else if (e.key == "a") {
         movePlayer("left");
+    } else if (e.key == "w" && e.key == "a") {
+        movePlayer("upper-left");
+        console.log("yes11");
     } else {
         console.log("yes");
     }
